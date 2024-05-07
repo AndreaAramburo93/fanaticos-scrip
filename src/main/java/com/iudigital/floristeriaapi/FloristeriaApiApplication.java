987f9a3095ea7df5de44e02a6beb8a64b8405de1
aleicer <aleicer.vesga@gmail.com>
2024-05-07 18:48:29 -0500

    Genere configuracion inicial para spring boot, dependencia jpa, mysql, dev tools y spring web

---
+++ src/main/java/com/iudigital/floristeriaapi/FloristeriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.iudigital.floristeriaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloristeriaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloristeriaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iudigital/floristeriaapi/FloristeriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iudigital.floristeriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloristeriaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
