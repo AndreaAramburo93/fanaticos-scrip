120e54bbbb5134c522fe8d96b079a18a777522c2
DaimerBaron <169172783+DaimerBaron@users.noreply.github.com>
2024-05-09 16:03:21 -0500

    configuracion de rutas para los link en nav

--- src/main/java/com/iudigital/floristeriaapi/controlador/FloristeriaControlador.java
+++ src/main/java/com/iudigital/floristeriaapi/controlador/FloristeriaControlador.java
@@ -1,5 +1,38 @@
 package com.iudigital.floristeriaapi.controlador;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class FloristeriaControlador {
-    
+
+    @GetMapping("/inventario")
+    public String inventario() {
+        return "inventario";
+    }
+    @GetMapping("/pedidos")
+    public String mostrarPedidos() {
+        return "pedidos"; // Nombre del archivo HTML correspondiente
+    }
+
+    @GetMapping("/arreglos")
+    public String mostrarArreglos() {
+        return "arreglos"; // Nombre del archivo HTML correspondiente
+    }
+
+    @GetMapping("/entregas")
+    public String mostrarEntregas() {
+        return "entregas"; // Nombre del archivo HTML correspondiente
+    }
+
+    @GetMapping("/finanzas")
+    public String mostrarFinanzas() {
+        return "finanzas"; // Nombre del archivo HTML correspondiente
+    }
+
+    @GetMapping("/informes")
+    public String mostrarInformes() {
+        return "informes"; // Nombre del archivo HTML correspondiente
+    }
 }
