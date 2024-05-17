1175374930d88fc382aceaa5bf7dcd803f1c87d7
AndreaAramburo93 <andrea.aramburo@est.iudigital.edu.co>
2024-05-17 18:15:16 -0500

    intento pedidos

---
+++ src/main/java/com/iudigital/floristeriaapi/models/PedidoModel.java
@@ -0,0 +1,67 @@
+package com.iudigital.floristeriaapi.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "pedido")
+
+public class PedidoModel {
+  @Id
+  @GeneratedValue (strategy= GenerationType.IDENTITY)
+
+private long id_pedido;
+private  String color_ramo;
+private String direccion_entrega;
+private String mensaje_compra;
+private String nombrecliente;
+private double precio_ramo;
+private int ramo;
+
+
+public long getId_pedido() {
+  return id_pedido;
+}
+public void setId_pedido(long id_pedido) {
+  this.id_pedido = id_pedido;
+}
+public String getColor_ramo() {
+  return color_ramo;
+}
+public void setColor_ramo(String color_ramo) {
+  this.color_ramo = color_ramo;
+}
+public String getDireccion_entrega() {
+  return direccion_entrega;
+}
+public void setDireccion_entrega(String direccion_entrega) {
+  this.direccion_entrega = direccion_entrega;
+}
+public String getMensaje_compra() {
+  return mensaje_compra;
+}
+public void setMensaje_compra(String mensaje_compra) {
+  this.mensaje_compra = mensaje_compra;
+}
+public String getNombrecliente() {
+  return nombrecliente;
+}
+public void setNombrecliente(String nombrecliente) {
+  this.nombrecliente = nombrecliente;
+}
+public double getPrecio_ramo() {
+  return precio_ramo;
+}
+public void setPrecio_ramo(double precio_ramo) {
+  this.precio_ramo = precio_ramo;
+}
+public int getRamo() {
+  return ramo;
+}
+public void setRamo(int ramo) {
+  this.ramo = ramo;
+}
+}
---
+++ src/main/java/com/iudigital/floristeriaapi/services/PedidosService.java
@@ -0,0 +1,51 @@
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iudigital.floristeriaapi.models.PedidoModel;
+import com.iudigital.floristeriaapi.repositories.IPedidoRepository;
+
+@Service
+public class PedidosService {
+
+  @Autowired
+  private IPedidoRepository pedidoRepository;
+
+  public PedidosService(IPedidoRepository pedidosRepository) {
+    this.pedidoRepository = pedidosRepository;
+  }
+
+  public List<PedidoModel> getAllPedidos() {
+    return this.pedidoRepository.findAll();
+  }
+
+  public PedidoModel savePedido(PedidoModel pedido) {
+    return this.pedidoRepository.save(pedido);
+  }
+
+  public Optional<PedidoModel> getPedidoById(Long id) {
+    return this.pedidoRepository.findById(id);
+  }
+
+  public PedidoModel updateById(PedidoModel pedido, long id) {
+    PedidoModel pedidoToUpdate = this.pedidoRepository.findById(id).get();
+    pedidoToUpdate.setNombrecliente(pedido.getNombrecliente());
+    pedidoToUpdate.setRamo(pedido.getRamo());
+    pedidoToUpdate.setColor_ramo(pedido.getColor_ramo());
+    pedidoToUpdate.setDireccion(pedido.getDireccion());
+    pedidoToUpdate.setMensaje_compra(pedido.getMensaje_compra());
+    pedidoToUpdate.setPrecio_ramo(pedido.getPrecio_ramo());
+    return this.pedidoRepository.save(pedidoToUpdate);
+  }
+
+  public Boolean deletePedido(Long id) {
+    try {
+      this.pedidoRepository.deleteById(id);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
